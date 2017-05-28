0cfa8dbb9445d75b4172e8aff7408a5f41aff95b
Lee Raulin <leeraulin@gmail.com>
2017-05-28 12:54:50 -0400

    fixed and separated headers

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-// init isotope
-$('.portfolio-items').isotope({
-  // options
-  itemSelector: '.portfolio-item',
-  layoutMode: 'fitRows'
-});
+$(document).ready(function() {
+
+  $("#header").load("header.html");
+  $("#footer").load("footer.html");
+
+  // init isotope
+  $('.portfolio-items').isotope({
+    // options
+    itemSelector: '.portfolio-item',
+    layoutMode: 'fitRows'
+  });
 
 
-//filter items on button click
-$(".portfolio-filter").on("click", "a", function(e) {
-  e.preventDefault();
-  var filterValue = $(this).attr("data-filter");
-  $('.portfolio-items').isotope({ filter: filterValue });
+  //filter items on button click
+  $(".portfolio-filter").on("click", "a", function(e) {
+    e.preventDefault();
+    var filterValue = $(this).attr("data-filter");
+    $('.portfolio-items').isotope({ filter: filterValue });
+  });
+
 });
